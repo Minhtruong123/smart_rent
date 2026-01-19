e50066d80709b498a6f2f57247fc4fecd141a1a9
Minhtruong123 <duongminhtruong1234@gmail.com>
2026-01-19 16:22:30 +0700

    init

---
+++ demo/src/main/java/com/cots/SmartrentApplication.java
@@ -0,0 +1,13 @@
+package com.cots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartrentApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/cots/SmartrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
